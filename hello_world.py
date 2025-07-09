4c4c27234388e553fae6caf5ff943d4c98390dd9
Bashar Alkhatib <116602051+githubBashar@users.noreply.github.com>
2025-07-09 09:00:26 +0200

    Feature: Alternierende Nachrichten hinzugefügt

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,13 @@
 # Dieses Skript gibt die Nachricht "Hello, World!" aus.
 # Es dient als einfaches Beispiel für ein Python-Programm.
 
-print("Hello, World!!")
+for i in range(20):
+    if i % 3 == 0:
+        for _ in range(5):
+            print("Hello, World!")
+    elif i % 3 == 1:
+        for _ in range(5):
+            print("Helloooo")
+    else:
+        for _ in range(5):
+            print("Hgello Wold")
